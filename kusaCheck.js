7f41fc4f40f1854a6e9a0d7767ca6117d23c7ef3
nonokaM <c1201071@gmail.com>
2023-04-24 23:32:00 +0900

    kusaCheck 日付の変換

--- kusaCheck.js
+++ kusaCheck.js
@@ -1,16 +1,18 @@
 function myFunction() {
-    let user = 'NonokaM';
-    let git_url = `https://github.com/users/${user}/contributions?from=2023-01-01`;
-    let response = UrlFetchApp.fetch(git_url);
-    let html = response.getContentText();
-    console.log(git_url);
-    console.log(html);
-    let hasContribution = html.includes("No contributions on Friday, April 21, 2023");
-    console.log(hasContribution)
-    
-    if (hasContribution) {
-      console.log("草生えてないよ");
-    } else {
-      console.log("草生えてるよ");
-    }
+
+  const formattedDate = new Date().toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+  console.log(formattedDate); // No contributions on Thursday, June 1, 2023
+
+  let user = 'NonokaM';
+  let git_url = `https://github.com/users/${user}/contributions?from=2023-01-01`;
+  let response = UrlFetchApp.fetch(git_url);
+  let html = response.getContentText();
+  let hasContribution = html.includes(`No contributions on ${formattedDate}`);
+  console.log(hasContribution)
+  
+  if (hasContribution) {
+    console.log("草生えてないよ");
+  } else {
+    console.log("草生えてるよ");
+  }
 }
