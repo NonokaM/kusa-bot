8c7a7cccd3cfc423333a8910a3cf97e6a5ecde62
nonokaM <c1201071@gmail.com>
2023-05-01 23:42:41 +0900

    add kusa

---
+++ checkContributions.js
@@ -0,0 +1,24 @@
+// 関数が実行されたとき、その日、Githubに草が生えているか否か判別する
+
+
+function checkContributions() {
+    const formattedDate = new Date().toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    console.log(formattedDate); // No contributions on Thursday, June 1, 2023
+
+    let user = 'NonokaM';
+    let git_url = `https://github.com/users/${user}/contributions`; // ?from=2023-01-01
+    let response = UrlFetchApp.fetch(git_url);
+    let html = response.getContentText();
+    let hasContribution = html.includes(`No contributions on ${formattedDate}`);
+    console.log(hasContribution)
+
+    if (hasContribution) {
+      console.log("草生えてないよ");
+      message = '草生えてないよ';
+    } else {
+      console.log("草生えてるよ");
+      message = '草生えてるよ';
+    }
+
+    return message;
+  }
