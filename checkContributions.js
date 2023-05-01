9084f93dafe191f8aff8806bedd377811ee9411f
nonokaM <c1201071@gmail.com>
2023-05-01 23:49:59 +0900

    Rename variable, add return value

--- checkContributions.js
+++ checkContributions.js
@@ -5,20 +5,21 @@ function checkContributions() {
     const formattedDate = new Date().toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
     console.log(formattedDate); // No contributions on Thursday, June 1, 2023
 
+    let contributionsMessage = '';
     let user = 'NonokaM';
     let git_url = `https://github.com/users/${user}/contributions`; // ?from=2023-01-01
     let response = UrlFetchApp.fetch(git_url);
     let html = response.getContentText();
-    let hasContribution = html.includes(`No contributions on ${formattedDate}`);
-    console.log(hasContribution)
+    let hasContributions = html.includes(`No contributions on ${formattedDate}`);
+    console.log(hasContributions)
 
-    if (hasContribution) {
+    if (hasContributions) {
       console.log("草生えてないよ");
-      message = '草生えてないよ';
+      contributionsMessage = '草生えてないよ';
     } else {
       console.log("草生えてるよ");
-      message = '草生えてるよ';
+      contributionsMessage = '草生えてるよ';
     }
 
-    return message;
+    return contributionsMessage;
   }
