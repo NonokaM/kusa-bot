3294f727e8cc9c34f25fd53d8feaa6edbf2375f3
nonokaM <c1201071@gmail.com>
2023-05-05 02:56:20 +0900

    add pushMessage

--- line.js
+++ line.js
@@ -1,45 +1,38 @@
 function replyMessage(token, replyToken, message) {
-    let url = 'https://api.line.me/v2/bot/message/reply';
+  let url = 'https://api.line.me/v2/bot/message/reply';
 
-    let payload = {
-      replyToken: replyToken,
-      messages: [message]
-    };
-    let options = {
-      payload: JSON.stringify(payload),
-      method: 'POST',
-      headers: {"Authorization" : "Bearer " + token},
-      contentType: 'application/json'
-    };
-    UrlFetchApp.fetch(url, options);
-  }
-
-
-  // const userId = eventData.source.userId;
-
-  // pushMessageText = "Ke";
-  // pushMessage(userId, token, pushMessageText);
-  // pushMessage();
+  let payload = {
+    replyToken: replyToken,
+    messages: [message]
+  };
+  let options = {
+    payload: JSON.stringify(payload),
+    method: 'POST',
+    headers: {"Authorization" : "Bearer " + token},
+    contentType: 'application/json'
+  };
+  UrlFetchApp.fetch(url, options);
+}
 
 
-  // function pushMessage(token, userId, pushMessageText) {
-  //   let push_url = 'https://api.line.me/v2/bot/message/push';
-  //   const message = {
-  //     type: 'text',
-  //     text: pushMessageText
-  //   };
-  //   const headers = {
-  //     'Content-Type': 'application/json',
-  //     'Authorization': 'Bearer ' + token,
-  //   };
-  //   const data = {
-  //     to: userId,
-  //     messages: [message],
-  //   };
-  //   const options = {
-  //     'method': 'post',
-  //     'headers': headers,
-  //     'payload': JSON.stringify(data),
-  //   };
-  //   UrlFetchApp.fetch(push_url, options);
-  // }
+function pushMessage(token, userId, pushMessageText) {
+  let push_url = 'https://api.line.me/v2/bot/message/push';
+  const message = {
+    type: 'text',
+    text: pushMessageText
+  };
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + token,
+  };
+  const data = {
+    to: userId,
+    messages: [message],
+  };
+  const options = {
+    'method': 'post',
+    'headers': headers,
+    'payload': JSON.stringify(data),
+  };
+  UrlFetchApp.fetch(push_url, options);
+}
