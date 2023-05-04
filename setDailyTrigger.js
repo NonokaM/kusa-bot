f5f401bc938829a61b7c838ba75c29bb238ba8aa
nonokaM <c1201071@gmail.com>
2023-05-05 04:43:41 +0900

    add pushContributions func

--- setDailyTrigger.js
+++ setDailyTrigger.js
@@ -1,27 +1,22 @@
-// GASで定期実行のトリガーを登録する
+// GASで定期実行のトリガーをつくる
 // 複数回実行して同じトリガーを増やさないよう注意
 
 
-// setDailyTrigger('22:32');
-
-
 function setDailyTrigger(kusaCheckTime) {
-    if (kusaCheckTime) {
-      var [hour, minute] = kusaCheckTime.split(":").map(str => parseInt(str));
-      ScriptApp.newTrigger("myFunctionToExecute")
-        .timeBased()
-        .atHour(hour)
-        .nearMinute(minute)
-        .everyDays(1)
-        .create();
-      console.log(hour, minute);
-    }
+  if (kusaCheckTime) {
+    var [hour, minute] = kusaCheckTime.split(":").map(str => parseInt(str));
+    ScriptApp.newTrigger("pushContributions")
+      .timeBased()
+      .atHour(hour)
+      .nearMinute(minute)
+      .everyDays(1)
+      .create();
   }
+}
 
-  function myFunctionToExecute() {
-    functionToExecute();
-  }
-
-  function functionToExecute() {
-    console.log('Hello');
-  }
+function pushContributions() {
+  let pushMessageText = checkContributions();
+  let properties = PropertiesService.getUserProperties();
+  let userId = properties.getProperty('key');
+  pushMessage(userId, pushMessageText);
+}
